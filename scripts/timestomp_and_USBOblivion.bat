f6f55ed5b8d57db91262ed3915412c17c8426d28
intchloe <chloe@cmail.nu>
2015-09-23 16:25:49 +0200

    Create timestomp_and_USBOblivion.bat

---
+++ scripts/timestomp_and_USBOblivion.bat
@@ -0,0 +1,9 @@
+cd %USERPROFILE%
+setlocal enabledelayedexpansion
+SET /A d=%RANDOM% %% 26 +2
+SET /A m=%RANDOM% %% 10 +2
+
+for /r %%a in (*) do (
+timestomp.exe "%%a" -z "Monday %m%/%d%/2015 2:55:55 AM"
+)
+USBOblivion64.exe -enable -auto -nosave -silent
